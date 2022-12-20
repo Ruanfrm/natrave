a76eaa1a6db9544e5ad146ef4224f33c8a90aa41
Ruan Freire <91690613+Ruanfrm@users.noreply.github.com>
2022-12-20 18:00:57 -0300

    Atualização new date

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -7,7 +7,7 @@ import {format, formatISO} from 'date-fns'
 
  
 export const Dashboard = () => {
-  const [currentDate, setDate] = useState(formatISO(new Date))
+  const [currentDate, setDate] = useState(formatISO(new Date(2022,11,20)))
   const [auth] = useLocalStorage('auth', {})
 
   const [hunches, fetchHunches] = useAsyncFn(async() => {
